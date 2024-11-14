2aa3a17ed87c4322b13406750961a94fd5064e81
Mouseman0919 <132811160+SoChunNing@users.noreply.github.com>
2024-11-14 17:41:23 +0800

    Update sound_syn.py
    Now the file can play the whole entire rythm of "世上只有媽媽好"

--- sound_syn.py
+++ sound_syn.py
@@ -5,14 +5,13 @@ import time
 def sound_synth():
     # Setup the OSC client
     client = udp_client.SimpleUDPClient("127.0.0.1", 57120)
-
-    # Frequency values to test
-    midinote = [82, 79, 77, 79, 84, 81, 79, 81, 0] #世上只有妈妈好
-
+    
+    midi_list = [81, 79, 76, 79, 84, 81, 79, 81, 76, 79, 81, 79, 76, 74, 72, 79,76, 74, 74, 74, 76, 79, 79, 81, 76, 74, 72, 79, 76, 74, 72, 69, 72, 67, 0]
     # Send each frequency value with a delay
-    for midi in midinote:
-        print(f"Sending Midinote Number: {midi} ")
-        client.send_message("/from_python", midi)
-        time.sleep(2)  # Wait for 2 seconds before sending the next frequency
+    
+    for midinote in midi_list:
+        print(f"Sending Midinote Number: {midinote} ")
+        client.send_message("/from_python", midinote)
+        time.sleep(1)
 
 sound_synth()
